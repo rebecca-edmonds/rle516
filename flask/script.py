3ffa06ab5dc435791434fd6cf5f01d1014c88b98
rebecca-edmonds <160595768+rebecca-edmonds@users.noreply.github.com>
2025-12-04 10:27:37 +0000

    Update script.py

--- flask/script.py
+++ flask/script.py
@@ -12,7 +12,7 @@ def index():
     df = pd.DataFrame({'enabled':[True, False],'code':["Y2025-abc", "Y2025-def"], "a":["x", "y"]})
     df['enabled'] = df['enabled'].apply(lambda x: '<input type="checkbox" name="one" value="one" checked>' if x else '<input type="checkbox" name="one" value="one" unchecked>')
     t = df.to_html(classes='data', escape=False, table_id="main_table")
-    return render_template('index.html', tables=[t], titles=df.columns.values)
+    return render_template('homepage.html', tables=[t], titles=df.columns.values)
   
 if __name__ == '__main__':
     app.run(debug=True)
